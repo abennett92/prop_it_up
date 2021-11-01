76d70e4710a565ed2c41f461d99d791a0469f7ec
abennett92 <saiford92@gmail.com>
2021-11-01 00:10:45 -0400

    quick bug fix

--- src/Components/PersonCard.jsx
+++ src/Components/PersonCard.jsx
@@ -3,6 +3,16 @@ import React, { Component } from 'react';
 
 class PersonCard extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            ageP: this.props.age
+        }
+    }
+
+    birthday = ()=>{
+        this.setState({age: this.state.ageP+1})
+    }
 
     render(){
         return(
@@ -11,6 +21,7 @@ class PersonCard extends Component{
                     <h1>{this.props.lastName}, {this.props.firstName}</h1>
                     <p>Age: {this.props.age}</p>
                     <p>Hair Color: {this.props.hairColor}</p>
+                    <p><button onClick = {this.birthday}>Happy Birthday!</button></p>
                 </div>
             </div>
         )
